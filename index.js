f3c666f815b7bcd9ced1b36e1752b519a0560259
Kat B <katreesebarnes@gmail.com>
2019-01-02 16:54:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,9 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) 
-  return "I can\'t hear you!"
+  return "I can't hear you!"
 }
 
-function sayHiToGrandma(string) {
-  if (string === string.toUpperCase())
-  return "YES INDEED!"
-}
+
+
 
