2693c71796b9be6388990a399e2c15e92b95f681
Kat B <katreesebarnes@gmail.com>
2019-01-02 17:13:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,10 @@ function sayHiToGrandma(string) {
   if (string === string.toUpperCase())
   return "YES INDEED!"
 }
-function sayHiToGrandma
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma."())
+  return "I love you, too."
+}
 
 
 
